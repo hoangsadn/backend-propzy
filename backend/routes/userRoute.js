343d203c9d8b5b66343f4f3328288796c8c9118e
truongvi <nltruongvi@gmail.com>
2021-01-25 11:38:38 +0700

    Update api

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -5,59 +5,61 @@ import { getToken, isAuth } from '../util';
 const router = express.Router();
 
 router.get('/:email', async (req, res) => {
-  const { email = '' } = req.params;
-  const users = await User.find({ email });
+	const { email = '' } = req.params;
+	const users = await User.find({ email });
 
-  const user = users[0];
+	const user = users[0];
 
-  if (user) {
-    res.send({
-      _id: user.id,
-      name: user.name,
-      email: user.email,
-      phone: user.phone,
-      turn: user.turn
-    });
-  } else {
-    res.send({ message: 'User Not Found' });
-  }
+	if (user) {
+		res.send({
+			_id: user.id,
+			name: user.name,
+			email: user.email,
+			phone: user.phone,
+			turn: user.turn
+		});
+	} else {
+		res.send({ message: 'User Not Found' });
+	}
 });
 
 router.put('/:email', async (req, res) => {
-  const { email = '' } = req.params;
-  const { turn = '' } = req.body;
+	const { email = '' } = req.params;
+	const { turn = '' } = req.body;
 
-  let userUpdate = await User.findOneAndUpdate({ email }, { turn: turn - 1 })
+	let userUpdate = await User.findOneAndUpdate({ email }, { turn: turn - 1 }, {
+		new: true
+	})
 
-  if (userUpdate) {
-    res.send({ ...userUpdate })
-  } else {
-    res.send({
-      message: "Can't not update"
-    })
-  }
+	if (userUpdate) {
+		res.send(userUpdate)
+	} else {
+		res.send({
+			message: "Can't not update"
+		})
+	}
 })
 
 router.post('/register', async (req, res) => {
-  console.log(req.body);
-  const user = new User({
-    name: req.body.name,
-    email: req.body.email,
-    phone: req.body.phone,
-    turn: 2,
-  });
+	console.log(req.body);
+	const user = new User({
+		name: req.body.name,
+		email: req.body.email,
+		phone: req.body.phone,
+		turn: 2,
+	});
 
-  const newUser = await user.save();
-  if (newUser) {
-    res.send({
-      _id: newUser.id,
-      name: newUser.name,
-      email: newUser.email,
-      phone: newUser.phone,
-    });
-  } else {
-    res.send({ message: 'Invalid User Data.' });
-  }
+	const newUser = await user.save();
+	if (newUser) {
+		res.send({
+			_id: newUser.id,
+			name: newUser.name,
+			email: newUser.email,
+			phone: newUser.phone,
+		});
+	} else {
+		res.send({ message: 'Invalid User Data.' });
+	}
 });
 
 export default router;
