65504dc141f22b0c4462324bd122957f842aaa6d
nltruongvi <nltruongvi@gmail.com>
2021-02-05 11:39:39 +0700

    Update all

--- backend/routes/sendmailRoute.js
+++ backend/routes/sendmailRoute.js
@@ -1,6 +1,7 @@
 import express from 'express';
 
 import nodemailer from 'nodemailer';
+import { EmailTemplate, EmailTemplate2, EmailTemplate3 } from '../../EmailTemplates/index.js';
 
 const router = express.Router();
 
@@ -68,64 +69,15 @@ const transporter = nodemailer.createTransport({
 
 router.post('/', (req, res) => {
   const {
-    email, name, user, coupon,
+    name, user, coupon,
   } = req.body;
-
-  const prize = prizes.find((nPrize) => nPrize.key === coupon.category);
-
-  const descriptions = prize.description.map((item) => `<li>${item}</li>`).join('');
 
   const mailOptions = {
     from: 'nltruongvi@gmail.com',
     to: user.email,
     subject: 'PROPZY COUPON',
     text: name,
-    html: `<!DOCTYPE html>
-        <html lang="en">
-        
-        <head>
-        
-        </head>
-        
-        <body>
-            <h3>Xin chào ${user.name},</h3>
-            <div>
-            Cảm ơn bạn đã tham gia chương trình CÓ NHÀ LÀ CÓ TẾT 2021 của Propzy được diễn ra từ ngày 25/01/2021 đến hết 28/02/2021.
-                <br/>
-                <br/>
-                Xin chúc mừng Bạn đã hái thành công Lì Xì – Khai Xuân Đón Lộc. Propzy xin gửi tặng Bạn phần quà đặc biệt mà bạn nhận được từ chương trình, vui lòng xem thêm bên dưới:
-            </div>
-            <div>
-            <br />
-               <font style="color:red;">${name}</font><br />
-               <div>${prize.detail}</div>
-               <ul>
-                ${descriptions}
-               </ul>
-            </div>
-            <div>
-            </br/>
-                Nếu <font style="color:red;">${user.name}</font> gặp bất kỳ khó khăn nào khi sử dụng Mã ưu đãi, vui lòng tham khảo thông tin dưới đây:
-            </div>
-            <ul>
-                <li>Sử dụng Ưu đãi của Lalamove: Gọi 0866869175 hoặc truy cập: <a href="https://www.lalamove.com/vietnam/hcmc/vi/home?utm_source=propzy">Link</a></li>
-                <li>Sử dụng Ưu đãi của Vietnam Moving: Gọi 18007011 hoặc truy cập: <a href="https://vietnammoving.com/?utm_source=propzy" >Link</a></li>
-                <li>Sử dụng Ưu đãi của Jupviec.vn: Gọi 19006082 hoặc truy cập:  <a href="https://www.jupviec.vn/huong-dan-dat-dich-vu-tong-ve-sinh-tren-ung-dung-jupviec-d2018.html">Link</a></li>
-                <li>Sử dụng Ưu đãi của GoDee: Gọi 0888411455 hoặc truy cập:  <a href="https://godee.vn/vn?utm_source=propzy">Link</a></li>
-                <li>Sử dụng Ưu đãi của HomeAZ: Gọi 090 173 2989 hoặc truy cập:  <a href="https://homeaz.vn/?utm_source=propzy">Link</a></li>
-            </ul>
-            <p>Propzy CARE – Gói dịch vụ chăm sóc khách hàng xuyên suốt trước, trong và cả sau khi giao dịch bất động sản tại Propzy. Với gói dịch vụ Propzy CARE, khách hàng <strong>được chăm sóc và hưởng đặc quyền ưu đãi miễn phí </strong> như vận chuyển, dọn nhà, cùng nhiều gói dịch vụ cộng thêm khác từ các đối tác của Propzy. Thảnh thơi Chill Tết - Propzy CARE hết!
-            Nhận ngay gói ưu đãi Propzy CARE trị giá 2.000.000 VNĐ từ ngày 25/01 - 28/02/2021 và hoàn tất giao dịch trước ngày 30/03/2021.       
-            </p>
-            <div>
-                Trụ sở chính: Tầng 4, toà nhà Flemington, 182 Lê Đại Hành, P.15, Q.11, TP.HCM.
-                <br />30 Trung tâm giao dịch: <a href="https://propzy.vn/diem-giao-dich">Xem Trung tâm giao dịch gần bạn</a>
-                <br />Liên hệ trực tiếp hỗ trợ viên của bạn
-                <br />CẦN HỖ TRỢ GỌI NGAY: <font style="color:red;">*4663</font>
-            </div>
-        </body>
-        
-        </html>`,
+    html: EmailTemplate({ user, coupon }),
   };
 
   transporter.sendMail(mailOptions, (error, info) => {
@@ -140,71 +92,11 @@ router.post('/', (req, res) => {
 router.post('/type2', (req, res) => {
   const { user, coupons = [] } = req.body;
 
-  const parseCounpons = coupons.map((coupon) => {
-    const prize = prizes.find((sprize) => sprize.key === coupon.category);
-
-    const descriptions = prize.description.map((item) => `<li>${item}</li>`).join('');
-
-    const string = ` <li>
-                <div style="color:red;">${coupon.category}: ${coupon.name}</div>
-                <div>${prize.detail}</div>
-                <ul>
-                    ${descriptions}
-                </ul> 
-            </li>`;
-
-    return string;
-  }).join('');
-
   const mailOptions = {
     from: 'nltruongvi@gmail.com',
     to: user.email,
     subject: 'PROPZY COUPON',
-    html: `<!DOCTYPE html>
-        <html lang="en">
-        
-        <head>
-        
-        </head>
-        
-        <body>
-            <h3>Xin chào ${user.name},</h3>
-            <div>
-            Cảm ơn bạn đã tham gia chương trình CÓ NHÀ LÀ CÓ TẾT 2021 của Propzy được diễn ra từ ngày 25/01/2021 đến hết 28/02/2021.
-                <br/>
-                <br/>
-                Xin chúc mừng Bạn đã hái thành công Lì Xì – Khai Xuân Đón Lộc. Propzy xin gửi tặng Bạn phần quà đặc biệt mà bạn nhận được từ chương trình, vui lòng xem thêm bên dưới:
-            </div>
-            <div>
-            <br />
-                <ul>
-               ${parseCounpons}
-                </ul>
-            </div>
-            <div>
-            </br/>
-                Nếu <font style="color:red;">${user.name}</font> gặp bất kỳ khó khăn nào khi sử dụng Mã ưu đãi, vui lòng tham khảo thông tin dưới đây:
-            </div>
-            <ul>
-                <li>Sử dụng Ưu đãi của Lalamove: Gọi 0866869175 hoặc truy cập: <a href="https://www.lalamove.com/vietnam/hcmc/vi/home?utm_source=propzy">Link</a></li>
-                <li>Sử dụng Ưu đãi của Vietnam Moving: Gọi 18007011 hoặc truy cập: <a href="https://vietnammoving.com/?utm_source=propzy" >Link</a></li>
-                <li>Sử dụng Ưu đãi của Jupviec.vn: Gọi 19006082 hoặc truy cập:  <a href="https://www.jupviec.vn/huong-dan-dat-dich-vu-tong-ve-sinh-tren-ung-dung-jupviec-d2018.html">Link</a></li>
-                <li>Sử dụng Ưu đãi của GoDee: Gọi 0888411455 hoặc truy cập:  <a href="https://godee.vn/vn?utm_source=propzy">Link</a></li>
-                <li>Sử dụng Ưu đãi của HomeAZ: Gọi 090 173 2989 hoặc truy cập:  <a href="https://homeaz.vn/?utm_source=propzy">Link</a></li>
-            </ul>
-            <p>Nhận ngay gói ưu đãi Propzy CARE trị giá 2.000.000 VNĐ từ ngày 25/01 - 28/02/2021 và hoàn tất giao dịch trước ngày 30/03/2021. </p>
-            </br/>
-            <p>Propzy CARE – Gói dịch vụ chăm sóc khách hàng xuyên suốt trước, trong và cả sau khi giao dịch bất động sản tại Propzy. Với gói dịch vụ Propzy CARE, khách hàng <strong>được chăm sóc và hưởng đặc quyền ưu đãi miễn phí </strong> như vận chuyển, dọn nhà, cùng nhiều gói dịch vụ cộng thêm khác từ các đối tác của Propzy. Thảnh thơi Chill Tết - Propzy CARE hết!
-            </p>
-            <div>
-                Trụ sở chính: Tầng 4, toà nhà Flemington, 182 Lê Đại Hành, P.15, Q.11, TP.HCM.
-                <br />30 Trung tâm giao dịch: <a href="https://propzy.vn/diem-giao-dich">Xem Trung tâm giao dịch gần bạn</a>
-                <br />Liên hệ trực tiếp hỗ trợ viên của bạn
-                <br />CẦN HỖ TRỢ GỌI NGAY: <font style="color:red;">*4663</font>
-            </div>
-        </body>
-        
-        </html>`,
+    html: EmailTemplate2({ user, coupons }),
   };
 
   transporter.sendMail(mailOptions, (error, info) => {
@@ -222,60 +114,7 @@ router.post('/type3', (req, res) => {
     from: 'nltruongvi@gmail.com',
     to: user.email,
     subject: 'PROPZY THƯ CẢM ƠN',
-    html: `<!DOCTYPE html>
-        <html lang="en">
-        
-        <head>
-        
-        </head>
-        
-        <body>
-            <div> Xin chào ${user.name} <div>
-            <br/>
-            
-            <div>
-                Cảm ơn bạn đã hoàn tất quá trình đăng ký tư vấn thuê nhà tại Propzy. Chúc bạn và người thân sẽ tận hưởng thêm
-                thật
-                nhiều khoảnh khắc vui vẻ và hạnh phúc trong năm mới.
-            </div>
-            <div>
-                *Lưu ý: Khi phát sinh giao dịch trước ngày 30/03/2021. Bạn sẽ nhận được gói ưu đãi Propzy CARE trị giá
-                2.000.000
-                VNĐ
-            </div>
-            <h4>
-                PROPZY CARE - ĐẶC QUYỀN ƯU ĐÃI DÀNH RIÊNG CHO KHÁCH HÀNG PROPZY
-
-            </h4>
-            <ul>
-                <li>Vietnam Moving - Giảm 500.000VNĐ cho khách đặt dịch vụ chuyển nhà trên website: vietnammoving.com
-                </li>
-                <li>HomeAZ - Giảm 600.000VNĐ dành cho khách hàng mua nệm tại website homeaz.vn
-                </li>
-                <li>Godee - Quà tặng lên đến 1.000.000VNĐ, miễn phí 5 chuyến đi cho tất cả khách hàng PROPZY và 20 chuyến đi cho khách hàng lần đầu sử dụng GoDee (tối đa 40.000VNĐ/chuyến)
-                </li>
-                <li>Lalamove - Giảm 75.000VNĐ cho khách đặt dịch vụ chuyển nhà bằng xe tải trên app Lalamove  </li>
-                <li>JupViec.vn - Giảm 100.000VNĐ cho dịch vụ tổng vệ sinh trên app JupViec.vn</li>
-            </ul>
-
-            <br/>
-            <br/>
-            <div>
-            Propzy CARE – Gói dịch vụ chăm sóc khách hàng xuyên suốt trước, trong và cả sau khi giao dịch bất động sản tại Propzy. Với gói dịch vụ Propzy CARE, khách hàng 
-            <b> được chăm sóc và hưởng đặc quyền ưu đãi miễn phí </b>
-             như vận chuyển, dọn nhà, cùng nhiều gói dịch vụ cộng thêm khác từ các đối tác của Propzy. Thảnh thơi Chill Tết - Propzy CARE hết!
-
-            </div>
-
-            <div>
-                Trụ sở chính: Tầng 4, toà nhà Flemington, 182 Lê Đại Hành, P.15, Q.11, TP.HCM.
-                <br />30 Trung tâm giao dịch: <a href="https://propzy.vn/diem-giao-dich">Xem Trung tâm giao dịch gần bạn</a> 
-                <br />Liên hệ trực tiếp hỗ trợ viên của bạn
-                <br />CẦN HỖ TRỢ GỌI NGAY: *4663
-            </div>
-        </body>
-        
-        </html>`,
+    html: EmailTemplate3({ user }),
   };
 
   transporter.sendMail(mailOptions, (error, info) => {
